d502176339ecad1785a739376fa7cb32d30daa53
r4z4 <r4z4aa@gmail.com>
2024-01-24 19:43:49 -0600

    pg updates

--- priv/repo/migrations/20230714163258_create_vector_extension.exs
+++ priv/repo/migrations/20230714163258_create_vector_extension.exs
@@ -2,10 +2,10 @@ defmodule FanCan.Repo.Migrations.CreateVectorExtension do
   use Ecto.Migration
 
   def up do
-    execute "CREATE EXTENSION IF NOT EXISTS vector"
+    # execute "CREATE EXTENSION IF NOT EXISTS vector"
   end
 
   def down do
-    execute "DROP EXTENSION vector"
+    # execute "DROP EXTENSION vector"
   end
 end
